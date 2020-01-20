20087c9a230f6cd8c2974737dfedd7771de9fc23
BryanYu <d58526@gmail.com>
2020-01-20 14:13:45 +0800

    EF Core GetStarted

---
+++ EFCoreSample/EFGetStarted/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace EFGetStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                Console.WriteLine("Inserting a new blog");
+                db.Add(new Blog() {Url = "http://blogs.msdn.com/dotnet"});
+                db.SaveChanges();
+
+                Console.WriteLine("Querying for a blog");
+                var blog = db.Blogs.OrderBy(b => b.BlogId).First();
+
+                Console.WriteLine("Updating the blog and adding a post");
+
+                blog.Url = "https://devblogs.microsoft.com/dotnet";
+                blog.Posts.Add(new Post()
+                {
+                    Title = "Hello world",
+                    Content = "I wrote an app using EF Core!"
+                });
+
+                db.SaveChanges();
+
+                Console.WriteLine("Delete the blog");
+                db.Remove(blog);
+                db.SaveChanges();
+
+            }
+
+        }
+    }
+}
